1a4812f51926cfb695578b0f938735b52758a200
richmondafari <afaririchmond1000@gmail.com>
2025-05-13 14:24:41 +0000

    feat(navbar): build responsive navbar with open/close functionality for mobile screens

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,20 @@
+'use client'
+
 import { assets } from "@/assets/assets";
 import Image from "next/image";
-import React from "react";
+import React, { useRef } from "react";
 
 const Navbar = () => {
+  const sideMenuRef = useRef();
+
+  const openMenu = () => {
+    sideMenuRef.current.style.transform = "translateX(-16rem)";
+  };
+
+  const closeMenu = () => {
+    sideMenuRef.current.style.transform = "translateX(16rem)";
+  };
+
   return (
     <>
       <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
@@ -70,7 +82,7 @@ const Navbar = () => {
               className="w-[10px] h-[10px] "
             />
           </a>
-          <button className="block md:hidden ml-3">
+          <button className="block md:hidden ml-3" onClick={openMenu}>
             <Image
               src={assets.menu_black}
               className="w-6 cursor-pointer"
@@ -78,6 +90,48 @@ const Navbar = () => {
             />
           </button>
         </div>
+
+        {/* Mobile Menu */}
+
+        <ul
+          ref={sideMenuRef}
+          className="flex md:hidden flex-col  gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500"
+        >
+          <li>
+            <div className="absolute right-6 top-6">
+              <Image
+                src={assets.close_black}
+                className="w-5 cursor-pointer mb-10"
+                onClick={closeMenu}
+                alt="close button"
+              />
+            </div>
+
+            <a href="#top" className="font-Ovo" onClick={closeMenu}>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="font-Ovo" onClick={closeMenu}>
+              About Me
+            </a>
+          </li>
+          <li>
+            <a href="#services" className="font-Ovo" onClick={closeMenu}>
+              Services
+            </a>
+          </li>
+          <li>
+            <a href="#work" className="font-Ovo" onClick={closeMenu}>
+              My Work
+            </a>
+          </li>
+          <li>
+            <a href="#contact" className="font-Ovo" onClick={closeMenu}>
+              Contact Me
+            </a>
+          </li>
+        </ul>
       </nav>
     </>
   );
