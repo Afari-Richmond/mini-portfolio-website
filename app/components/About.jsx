ef4cb2c4743dee6fb7393c6a1fee27df95487fa6
richmondafari <afaririchmond1000@gmail.com>
2025-05-30 10:46:33 +0000

    feat(about): build the about section page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { assets } from "@/assets/assets";
+import { assets, infoList, toolsData } from "@/assets/assets";
 
 const About = () => {
   return (
@@ -23,12 +23,33 @@ const About = () => {
             Nobis doloremque minus officia dolorum sit iure alias ab velit ea,
             consectetur dolore! Veniam distinctio, officia eius eaque minus
             maiores labore velit cum nihil quam molestiae consequuntur repellat!
-            
-
-            <ul>
-                
-            </ul>
           </p>
+
+          <ul className="grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl ">
+            {infoList.map(({ icon, iconDark, title, description }, index) => (
+              <li
+                key={index}
+                className="  border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:box-shadow: 4px 4px 0 #000"
+              >
+                <Image src={icon} alt={title} className="w-7 mt-3" />
+                <h3 className="my-4 font-semibold text-gray-700"> {title}</h3>
+                <p className="text-gray-600 text-sm">{description}</p>
+              </li>
+            ))}
+          </ul>
+
+          <h4 className="my-6 text-gray-700 font-Ovo">Tools I use</h4>
+
+          <ul className="flex items-3 gap-3 sm:gap-5">
+            {toolsData.map((tool, index) => (
+              <li
+                key={index}
+                className="flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500 hover:box-shadow: 4px 4px 0 #000"
+              >
+                <Image src={tool} alt="Tool" className="w-5 sm:w-7" />
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,21 +3,24 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-        colors:{
-            lightHover: '#fcf4ff',
-            darkHover: '#2A004A',
-            darkTheme: '#11001f'
+      colors: {
+        lightHover: "#fcf4ff",
+        darkHover: "#2A004A",
+        darkTheme: "#11001f",
+      },
 
-        },
-
-        fontFamily: {
-            outFit: ['Outfit', 'sans-serif'],
-            Ovo: ['Ovo', 'serif'],
-        }
+      fontFamily: {
+        outFit: ["Outfit", "sans-serif"],
+        Ovo: ["Ovo", "serif"],
+      },
+      boxShadow: {
+        black: "4px 4px 0 #000",
+        white: "4px 4px 0 #fff",
+      },
     },
   },
   plugins: [],
